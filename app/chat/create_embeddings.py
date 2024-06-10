8d54752a167b97f096918a61cd402fe93e0aa02a
Jarvisiscool <ni7thetruegoat@gmail.com>
2024-06-10 14:41:12 -0500

    git config --get "Jarvisiscool"

--- app/chat/create_embeddings.py
+++ app/chat/create_embeddings.py
@@ -1,5 +1,6 @@
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from app.chat.vector_stores.pinecone import vector_store
 
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     text_splitter = RecursiveCharacterTextSplitter(
