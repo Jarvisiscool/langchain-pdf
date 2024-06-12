9096c56dabbb4aa547e0d8868dffecfc850de245
Jarvisiscool <ni7thetruegoat@gmail.com>
2024-06-12 15:43:33 -0500

    git config --get commit.template

---
+++ app/chat/memories/sql_memory.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import BaseChatMessageHistory
+
+from app.web.api import (
+    get_messages_by_conversation_id, 
+    add_message_to_conversation
+)
