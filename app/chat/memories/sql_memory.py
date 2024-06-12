40c93a0166a51fd24db94cfc0db548e02a96ff9b
Jarvisiscool <ni7thetruegoat@gmail.com>
2024-06-12 16:03:25 -0500

    git config --get commit.template

--- app/chat/memories/sql_memory.py
+++ app/chat/memories/sql_memory.py
@@ -27,10 +27,8 @@ class SqlMessageHistory(BaseChatMessageHistory, BaseModel):
     
 def build_memory(chat_args):
     return ConversationBufferMemory(
-        chat_memory=SqlMessageHistory(
-            conversation_id=chat_args.conversation_id
-            ),
+        chat_memory=SqlMessageHistory(conversation_id=chat_args.conversation_id),
         return_messages=True,
-        memory="chat_history",
+        memory_key="chat_history",
         output_key="answer"
     )
