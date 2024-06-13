c4ff178c93d7e7f1507abff1dacd8ef6a83d7710
Jarvisiscool <ni7thetruegoat@gmail.com>
2024-06-13 16:45:55 -0500

    git config --get commit.template

--- app/chat/chains/streamable.py
+++ app/chat/chains/streamable.py
@@ -7,16 +7,15 @@ class StreamableChain:
     def stream(self, input):
         queue = Queue()
         handler = StreamingHandler(queue)
-        
-        
-        def task():
+
+        def task(app_context):
             app_context.push()
             self(input, callbacks=[handler])
-        
+
         Thread(target=task, args=[current_app.app_context()]).start()
         
         while True:
-            token= queue.get()
+            token = queue.get()
             if token is None:
                 break
             yield token
