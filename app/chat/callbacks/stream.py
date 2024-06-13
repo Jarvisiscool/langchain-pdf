5fb1f2c546cffc9e02be302400c51616ff47e3a3
Jarvisiscool <ni7thetruegoat@gmail.com>
2024-06-13 16:57:37 -0500

    git config --get commit.template

--- app/chat/callbacks/stream.py
+++ app/chat/callbacks/stream.py
@@ -3,7 +3,11 @@ from langchain.callbacks.base import BaseCallbackHandler
 class StreamingHandler(BaseCallbackHandler):
     def __init__(self, queue):
         self.queue = queue
-
+    
+    def on_chat_model_start(self, serialized, messages, run_id, **kwargs):
+        print(serialized)
+        print(run_id)
+    
     def on_llm_new_token(self, token, **kwargs):
         self.queue.put(token)
 
