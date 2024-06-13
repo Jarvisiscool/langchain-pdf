4f6f972cd2bf77dd68a627c3db12a6734ff20f12
Jarvisiscool <ni7thetruegoat@gmail.com>
2024-06-13 16:29:39 -0500

    git config --get commit.template

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -2,7 +2,7 @@ from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.llm.chatopenai import build_llm
 from app.chat.memories.sql_memory import build_memory
-from app.chat.chains.streamable import StreamingConversationalRetrievalChain
+from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 
 def build_chat(chat_args: ChatArgs):
     retriever = build_retriever(chat_args)
