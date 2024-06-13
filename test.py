88a99ae2687332e84ec420baa9472149bdc74952
Jarvisiscool <ni7thetruegoat@gmail.com>
2024-06-13 13:36:10 -0500

    git config --get commit.template

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 from langchain.chat_models import ChatOpenAI 
 from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+from langchain.callbacks.base import BaseCallbackHandler
 from dotenv import load_dotenv
 
 load_dotenv()
 
-chat = ChatOpenAI(streaming=True)
+class StreamingHandler(BaseCallbackHandler):
+    def on_llm_new_token(self, token, **kwagrs):
+        print(token)
+
+
+chat = ChatOpenAI(streaming=True, callbacks=[StreamingHandler()])
 
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -12,7 +19,11 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
+chain = LLMChain(chat=chat, prompt=prompt)
+output = chain("tell me a joke")
+print(output)
+
 messages = prompt.format_messages(content="tell me a joke")
 
 for message in chat.stream(messages):
-    print(message)
+    print(message.content)
