90846c6c019e36256cf3f7e10fb7a6d890fa5c60
Jarvisiscool <ni7thetruegoat@gmail.com>
2024-06-13 17:01:10 -0500

    git config --get commit.template

--- app/chat/callbacks/stream.py
+++ app/chat/callbacks/stream.py
@@ -5,8 +5,8 @@ class StreamingHandler(BaseCallbackHandler):
         self.queue = queue
     
     def on_chat_model_start(self, serialized, messages, run_id, **kwargs):
-        print(serialized)
-        print(run_id)
+        if serialized["kwargs"]["streaming"]:
+            print("This is a streaming model! I should listen to events with a run_id of", run_id)
     
     def on_llm_new_token(self, token, **kwargs):
         self.queue.put(token)
