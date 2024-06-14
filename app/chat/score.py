d1747e27e5c4450a91e477724c0443e79782fdbf
Jarvisiscool <ni7thetruegoat@gmail.com>
2024-06-13 21:57:54 -0500

    git config --get commit.template

--- app/chat/score.py
+++ app/chat/score.py
@@ -1,8 +1,39 @@
 from app.chat.redis import client
+import random
 
 
 def random_component_by_score(component_type, component_map):
-    pass
+    #Make sure component type is 'llm', retriever, memory
+    if component_type not in ["llm", "retriever", "memory"]:
+        raise ValueError("Invalid component_type")
+        
+    #From redis, get the hash containing the sum total scores for given component_type
+    values = client.hgetall(f"{component_type}_score_values")
+    
+    #From redis, get the hash containing the number of times each component has been voted on
+    counts = client.hgetall(f"{component_type}_score_counts")
+    
+    #Get all the valid component names from the component map
+    names = component_map.keys()
+    
+    #Loop over those valid names and use them to calculate the average score for each
+    #Add average scores and store into a dictionary
+    avg_scores = {}
+    for name in names:
+        score = int(values.get(name, 1))
+        count = int(counts.get(name, 1))
+        avg = score / count
+        avg_scores[name] = max(avg, 0.1)
+    
+    #Do a weighted random selection
+    sum_scores = sum(avg_scores.values())
+    random_val = random.uniform(0,sum_scores)
+    cummulative = 0
+    for name, score in avg_scores.items():
+        cummulative += score
+        if random_val <= cummulative:
+            return name
+    
 
 def score_conversation(
     conversation_id: str, score: float, llm: str, retriever: str, memory: str
