8184ae4f07acb2be3c239f39833215ca6750e679
Jarvisiscool <ni7thetruegoat@gmail.com>
2024-06-13 22:47:37 -0500

    git config --get commit.template

--- app/chat/tracing/langfuse.py
+++ app/chat/tracing/langfuse.py
@@ -3,6 +3,7 @@ from langfuse.client import Langfuse
 
 langfuse = Langfuse(
     os.environ["LANGFUSE_PUBLIC_KEY"],
-    os.environ["LANGFUSE_SECRET_KEY"]
+    os.environ["LANGFUSE_SECRET_KEY"],
+    host="https://us.cloud.langfuse.com/"
 )
 
