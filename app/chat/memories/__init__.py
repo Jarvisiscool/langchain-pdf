9351a7571cdebaaecf71b02612d5e5fd5207dd5a
Jarvisiscool <ni7thetruegoat@gmail.com>
2024-06-17 13:42:08 -0500

    e

--- app/chat/memories/__init__.py
+++ app/chat/memories/__init__.py
@@ -1,4 +1,5 @@
 from .sql_memory import build_memory
+#Allows you to disregard info that is not related to question asked and only takes memory that is relevant
 from .window_memory import window_buffer_window_memory
 
 #Dictionary that holds keys for type of memory
