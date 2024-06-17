6df983bf0d9deac0f974a91bc87e41c8a0c416a8
Jarvisiscool <ni7thetruegoat@gmail.com>
2024-06-17 10:52:04 -0500

    e

--- app/chat/redis.py
+++ app/chat/redis.py
@@ -4,5 +4,6 @@ import redis
 #Easier to work with
 client = redis.Redis.from_url(
     os.environ["REDIS_URI"],
+    #Redis gives bytes, decodes it into to strings; easier to read
     decode_responses=True
 )
