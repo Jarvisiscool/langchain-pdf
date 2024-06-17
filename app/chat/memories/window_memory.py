ea8a649123f42fe642a7642ba4d59355bbb370c4
Jarvisiscool <ni7thetruegoat@gmail.com>
2024-06-17 13:48:39 -0500

    e

--- app/chat/memories/window_memory.py
+++ app/chat/memories/window_memory.py
@@ -6,6 +6,7 @@ def window_buffer_window_memory(chat_args):
         memory_key = "chat_history",
         output_key = "answer",
         return_messages = True,
+        #Same as ConversationBufferMemory expcept that the k allows you to regulate the amount of exchanges you need to extract from SqlMessageHistory
         chat_memory = SqlMessageHistory(
             conversation_id = chat_args.conversation_id
         ),
