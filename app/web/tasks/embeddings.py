a6d3513b439a06881b8a4346be29e333780c7112
Jarvisiscool <ni7thetruegoat@gmail.com>
2024-07-16 17:42:16 -0500

    e

--- app/web/tasks/embeddings.py
+++ app/web/tasks/embeddings.py
@@ -10,4 +10,4 @@ def process_document(pdf_id: int):
     pdf = Pdf.find_by(id=pdf_id)
     with download(pdf.id) as pdf_path:
         create_embeddings_for_pdf(pdf.id, pdf_path)
-    print(create_embeddings_for_pdf(pdf.id, pdf_path))
+    
